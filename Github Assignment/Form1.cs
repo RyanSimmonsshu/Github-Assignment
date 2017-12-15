c66510eb03433dfe4fcf436bf3dbf8a9cb790e1f
Simmons <b7008442@hallam.shu.ac.uk>
2017-12-15 16:01:07 +0000

    Add project files.

---
+++ Github Assignment/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Github_Assignment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void picb_1p_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picb_2p_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picb_5p_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picb_10p_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picb_20p_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picb_50p_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picb_100p_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picb_200p_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void reset_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
