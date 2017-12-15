a65402dfeaa40f002a8641941f660e281d6e76a0
b7008442 <b7008442@9310-10J522010>
2017-12-15 16:21:22 +0000

    testing

--- Github Assignment/Form1.cs
+++ Github Assignment/Form1.cs
@@ -18,48 +18,148 @@ namespace Github_Assignment
         }
 
         private void picb_1p_Click(object sender, EventArgs e)
+        //This peice of code is used to say if the "picture box" named "picb_1p" is clicked then start the following lines of code.
         {
-
+             if (txtb_costpercredit.Text == "0" || txtb_costpercredit.Text =="")
+                //This peice of code is used to say if the "text box" "txtb_costpercredit" "Text property" is set to 0 or empty then it will execute the next line of code.
+            {
+                MessageBox.Show("Please input a cost per credit");
+                //This peice of code creates a message which will appear in a seperate window stating that the user needs to input a cost per credit into the "textbox" "txtb_cospercredit" 
+                //hi
+            }
+            else
+            //else means if the data within the "texbox" "text property" isnt 0 or empty it will allow the folowing code to run and you wont be stopped by the message box.
+            {
+              
+            }
         }
 
         private void picb_2p_Click(object sender, EventArgs e)
         {
+            if (txtb_costpercredit.Text == "0" || txtb_costpercredit.Text == "")
+            //This peice of code is used to say if the "text box" "txtb_costpercredit" "Text property" is set to 0 or empty then it will execute the next line of code.
+            {
+                MessageBox.Show("Please input a cost per credit");
+                //This peice of code creates a message which will appear in a seperate window stating that the user needs to input a cost per credit into the "textbox" "txtb_cospercredit" 
 
+            }
+            else
+            //else means if the data within the "texbox" "text property" isnt 0 or empty it will allow the folowing code to run and you wont be stopped by the message box.
+            {
+
+            }
         }
 
         private void picb_5p_Click(object sender, EventArgs e)
         {
+            if (txtb_costpercredit.Text == "0" || txtb_costpercredit.Text == "")
+            //This peice of code is used to say if the "text box" "txtb_costpercredit" "Text property" is set to 0 or empty then it will execute the next line of code.
+            {
+                MessageBox.Show("Please input a cost per credit");
+                //This peice of code creates a message which will appear in a seperate window stating that the user needs to input a cost per credit into the "textbox" "txtb_cospercredit" 
 
+            }
+            else
+            //else means if the data within the "texbox" "text property" isnt 0 or empty it will allow the folowing code to run and you wont be stopped by the message box.
+            {
+
+            }
         }
 
         private void picb_10p_Click(object sender, EventArgs e)
         {
+            if (txtb_costpercredit.Text == "0" || txtb_costpercredit.Text == "")
+            //This peice of code is used to say if the "text box" "txtb_costpercredit" "Text property" is set to 0 or empty then it will execute the next line of code.
+            {
+                MessageBox.Show("Please input a cost per credit");
+                //This peice of code creates a message which will appear in a seperate window stating that the user needs to input a cost per credit into the "textbox" "txtb_cospercredit" 
 
+            }
+            else
+            //else means if the data within the "texbox" "text property" isnt 0 or empty it will allow the folowing code to run and you wont be stopped by the message box.
+            {
+
+            }
         }
 
         private void picb_20p_Click(object sender, EventArgs e)
         {
+            if (txtb_costpercredit.Text == "0" || txtb_costpercredit.Text == "")
+            //This peice of code is used to say if the "text box" "txtb_costpercredit" "Text property" is set to 0 or empty then it will execute the next line of code.
+            {
+                MessageBox.Show("Please input a cost per credit");
+                //This peice of code creates a message which will appear in a seperate window stating that the user needs to input a cost per credit into the "textbox" "txtb_cospercredit" 
 
+            }
+            else
+            //else means if the data within the "texbox" "text property" isnt 0 or empty it will allow the folowing code to run and you wont be stopped by the message box.
+            {
+
+            }
         }
 
         private void picb_50p_Click(object sender, EventArgs e)
         {
+            if (txtb_costpercredit.Text == "0" || txtb_costpercredit.Text == "")
+            //This peice of code is used to say if the "text box" "txtb_costpercredit" "Text property" is set to 0 or empty then it will execute the next line of code.
+            {
+                MessageBox.Show("Please input a cost per credit");
+                //This peice of code creates a message which will appear in a seperate window stating that the user needs to input a cost per credit into the "textbox" "txtb_cospercredit" 
 
+            }
+            else
+            //else means if the data within the "texbox" "text property" isnt 0 or empty it will allow the folowing code to run and you wont be stopped by the message box.
+            {
+
+            }
         }
 
         private void picb_100p_Click(object sender, EventArgs e)
         {
+            if (txtb_costpercredit.Text == "0" || txtb_costpercredit.Text == "")
+            //This peice of code is used to say if the "text box" "txtb_costpercredit" "Text property" is set to 0 or empty then it will execute the next line of code.
+            {
+                MessageBox.Show("Please input a cost per credit");
+                //This peice of code creates a message which will appear in a seperate window stating that the user needs to input a cost per credit into the "textbox" "txtb_cospercredit" 
 
+            }
+            else
+            //else means if the data within the "texbox" "text property" isnt 0 or empty it will allow the folowing code to run and you wont be stopped by the message box.
+            {
+
+            }
         }
 
         private void picb_200p_Click(object sender, EventArgs e)
         {
+            if (txtb_costpercredit.Text == "0" || txtb_costpercredit.Text == "")
+            //This peice of code is used to say if the "text box" "txtb_costpercredit" "Text property" is set to 0 or empty then it will execute the next line of code.
+            {
+                MessageBox.Show("Please input a cost per credit");
+                //This peice of code creates a message which will appear in a seperate window stating that the user needs to input a cost per credit into the "textbox" "txtb_cospercredit" 
 
+            }
+            else
+            //else means if the data within the "texbox" "text property" isnt 0 or empty it will allow the folowing code to run and you wont be stopped by the message box.
+            {
+
+            }
         }
 
         private void reset_Click(object sender, EventArgs e)
         {
+            if (txtb_costpercredit.Text == "0" || txtb_costpercredit.Text == "")
+            //This peice of code is used to say if the "text box" "txtb_costpercredit" "Text property" is set to 0 or empty then it will execute the next line of code.
+            {
+                MessageBox.Show("Please input a cost per credit");
+                //This peice of code creates a message which will appear in a seperate window stating that the user needs to input a cost per credit into the "textbox" "txtb_cospercredit" 
 
+            }
+            else
+            //else means if the data within the "texbox" "text property" isnt 0 or empty it will allow the folowing code to run and you wont be stopped by the message box.
+            {
+
+            }
         }
     }
 }
